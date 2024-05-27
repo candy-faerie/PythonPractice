80973d3ee5be2dc9c54d0e6845672b8a7506da04
candy-faerie <110663596+candy-faerie@users.noreply.github.com>
2024-05-27 20:51:54 +0900

    find the average value of dice
    한 개의 주사위를 5번 던져서 나온 수 들의 평균 구하

---
+++ find the average value of dice.py
@@ -0,0 +1,23 @@
+
+import random
+
+def add(d1,d2,d3,d4,d5):
+    puls=d1+d2+d3+d4+d5
+    mean=puls/5
+    print ('주사위 값 :[',dice_1,dice_2,dice_3,dice_4,dice_5,']')
+    print('5개 주사위 값의 평균 : ',mean)
+
+dice_1=random.randint(1,6)
+dice_2=random.randint(1,6)
+dice_3=random.randint(1,6)
+dice_4=random.randint(1,6)
+dice_5=random.randint(1,6)
+    
+
+add(dice_1,dice_2,dice_3,dice_4,dice_5)
+
+
+
+
+
+    
